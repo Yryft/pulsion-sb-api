40a4821a21bfee976a5835f69b05fb532798bb8c
GoldenGhostX <golden6ghost@gmail.com>
2025-05-15 11:42:26 +0200

    Update session.py

--- db/session.py
+++ db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL",
-    'postgresql://postgres:cFwXnePinMlwySESPTqigzWzqeAHKcVt@switchyard.proxy.rlwy.net:41004/railway')
+    'DATABASE_URL')
 
 # Create engine
 engine = create_engine(DATABASE_URL, pool_pre_ping=True, echo=True)
